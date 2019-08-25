984d492e46c6fff91babdb088238944ea528adbe
WilsonMariano <mgw009@gmail.com>
2019-08-25 00:37:48 -0300

    Se crean componentes compartidos como navbar, sidebar, breadcrumbs; componente login y nopagefound. Se instala boostrap y jquery, se agregan estilos para navbar y sidebar. Se suben recursos, librerias e imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule

c77478cf3c6d11b1619240c02170306c69985e1b
WilsonMariano <mgw009@gmail.com>
2019-10-13 15:00:49 -0300

    Cambios en alta de gasto

--- src/app/pages/datos-gastos-expensa/datos-gastos-expensa.component.ts
+++ src/app/pages/datos-gastos-expensa/datos-gastos-expensa.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ConceptosGastosService, DiccionarioService, FxGlobalsService, CommonService } from 'src/app/services/service.index';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, Validators, FormBuilder } from '@angular/forms';
 
 declare var $ : any;
 
@@ -12,29 +12,35 @@ declare var $ : any;
 export class DatosGastosExpensaComponent implements OnInit {
 
   public forma: FormGroup;
+  public amountRows = 0;
   public arrEntidades = [];
   public arrManzanas  = [];
 
-  constructor( private _conceptoGastos: ConceptosGastosService, private _diccionario: DiccionarioService, private _fxGlobals: FxGlobalsService, private _common: CommonService ) { }
+  constructor( private formBuilder: FormBuilder, private _conceptoGastos: ConceptosGastosService, private _diccionario: DiccionarioService, private _fxGlobals: FxGlobalsService, private _common: CommonService ) { }
 
 
   ngOnInit() {  
 
-    this.forma = new FormGroup({
-      'concepto': new FormControl( { value: '', disabled: true }, Validators.required ),
-      'monto': new FormControl( '', Validators.required ),
-      'descripcion': new FormControl( '', Validators.required ),
-      'entidad': new FormControl( '', Validators.required )
+    this.forma = this.formBuilder.group({
+      controls: new FormArray([])
     });
+
 
     this.getEntidades();
     this.getManzanas();
 
+    this.addRow();
+
   }
 
+   // convenience getters for easy access to form fields
+  //  get f() { return this.forma.controls; }
+   get t() { return this.forma.controls.controls as FormArray; }
+  //  private getControl(i) { return this.t.controls[i].controls as  }
 
 
-  public pressCodigo( event ): void {
+
+  public pressCodigo( event, index ): void {
 
     let value: String = event['srcElement'].value;
 
@@ -44,7 +50,8 @@ export class DatosGastosExpensaComponent implements OnInit {
       console.log(value);
       this._conceptoGastos.getOne( value ).subscribe(
 
-        data => this.forma.get('concepto').setValue(data.conceptoGasto),
+        data => this.t.controls[index].controls['concepto'].setValue(data.conceptoGasto),
+        // data => this.getControl['concepto'].setValue(data.conceptoGasto),
         err => this.forma.get('concepto').reset()
       );
     }
@@ -72,13 +79,50 @@ export class DatosGastosExpensaComponent implements OnInit {
 
 
 
-  public onChangeEntidad() : void {
+  public onChangeEntidad(i) : void {
 
-    console.log(this.forma.get('entidad').value);
+    let entidad = this.forma.get('controls')['controls'][i].get('entidad').value;
+
+
+    switch(entidad) {
+
+      case 'TIPO_ENTIDAD_1':
+          this.forma.get('controls')['controls'][i].addControl('140', new FormControl(''));
+          this.forma.get('controls')['controls'][i].addControl('141', new FormControl(''));
+          this.forma.get('controls')['controls'][i].addControl('142', new FormControl(''));
+          this.forma.get('controls')['controls'][i].addControl('143', new FormControl(''));
+        break;
+
+      default:
+        this.forma.get('controls')['controls'][i].addControl('numero', new FormControl(''));
+        break;
+
+    }
   }
 
 
-  public agregarGasto() {
-    console.log("Agregado");
+test(){
+
+  console.log(this.forma);
+}
+
+  public addRow() {
+
+    
+
+    this.amountRows ++;
+
+        for (let i = this.t.length; i < this.amountRows; i++) {
+            this.t.push(this.formBuilder.group({
+                concepto: [{ value: '', disabled: true }, Validators.required],
+                monto: ['', Validators.required],
+                descripcion: [''],
+                entidad: ['', Validators.required],
+                valores: ['', Validators.required]
+            }));
+        }
+  
+        
   }
+
 }
